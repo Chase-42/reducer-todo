21624c81cfe00c128320e1a620aea72e0fb5ae17
Chase-42 <chaseofthecollins@gmail.com>
2020-07-02 15:49:18 -0600

    Changed header to h1, added touch of margin-top to application, made em's to rem's

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Todo from "./components/Todo";
-import styled from "styled-components";
-import "./App.css";
+import React from 'react';
+import Todo from './components/Todo';
+import styled from 'styled-components';
+import './App.css';
 
 function App() {
   return (
     <AppDiv>
-      <h2>Welcome to your Todo App!</h2>
+      <h1>Welcome to your To Do App!</h1>
       <Todo />
     </AppDiv>
   );
@@ -18,9 +18,9 @@ const AppDiv = styled.div`
   background: #6a8b67;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12), 0 5px 2px rgba(0, 0, 0, 0.24);
   width: 100%;
-  height: 58em;
-  max-width: 50em;
-  margin: 16px auto;
+  height: 58rem;
+  max-width: 50rem;
+  margin: 2.25rem auto;
   text-align: center;
   display: flex;
   flex-direction: column;
