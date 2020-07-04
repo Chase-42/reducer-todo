d443142bd5eb0e9764407aec85dd646365f28352
Chase-42 <chaseofthecollins@gmail.com>
2020-07-04 11:54:39 -0600

    mobile view tweaks

--- src/App.js
+++ src/App.js
@@ -17,10 +17,8 @@ export default App;
 const AppDiv = styled.div`
   background: #6a8b67;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12), 0 5px 2px rgba(0, 0, 0, 0.24);
-  width: 100%;
   height: 58rem;
-  max-width: 50rem;
-  margin: 2.25rem auto;
+  margin: 2% 5% 5% 5%;
   text-align: center;
   display: flex;
   flex-direction: column;
